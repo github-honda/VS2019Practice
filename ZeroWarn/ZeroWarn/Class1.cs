9b0c903bbb3a93f6a815c43af9f3048743799f58
Honda <hondachen@hotmail.com>
2020-06-04 11:32:17 +0800

    Add const samples

--- ZeroWarn/ZeroWarn/Class1.cs
+++ ZeroWarn/ZeroWarn/Class1.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+
+Summary:
+1. Fieldtest1 is OK for naming rule. (fieldtest1, _fieldtest1 is NOT OK).
+2. Const can begin with lower case characters and Prefix '_'. Why ?
+
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,6 +25,14 @@ namespace ZeroWarn
         // IDE1006 Naming rule violation: These words must begin with upper case characters.
         public string sName { get; set; }
 
+        // IDE0049 Name can be simplified. Because of "Boolean".
+        public const Boolean bDebug = true;
+
+        // To fix IDE0049 Name can be simplified. Because of "Boolean".
+        // Why const can begin with lower case characters and Prefix '_' ?
+        public const bool bDebugOk = true;
+        public const bool _bDebugOk = true;
+
         public void Method1() // OK for Naming rule.
         { }
         public void MethodTest1() // OK for Naming rule.
@@ -27,7 +42,10 @@ namespace ZeroWarn
 
         // IDE1006 Naming rule violation: These words must begin with upper case characters.
         public void method1()
-        { }
+        {
+            if (bDebug)
+                Field1 = "dummy";
+        }
 
         // IDE1006 Naming rule violation: These words must begin with upper case characters.
         public void methodtest1()
